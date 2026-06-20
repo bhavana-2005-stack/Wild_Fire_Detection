a5bea63b48c59851522cf657bac5e6d86696ed80
Bhavana <bhavana2005@gmail.com>
2026-06-20 12:29:13 +0530

    Use Hugging Face default port

--- app.py
+++ app.py
@@ -308,5 +308,5 @@ if __name__ == '__main__':
         print(rule)
     print("========================")
 
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 7860))
     app.run(debug=False, host='0.0.0.0', port=port)
